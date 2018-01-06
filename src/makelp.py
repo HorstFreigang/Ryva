f90106277f76f2b71c8f0c4db652ea6a2a395e1f
Horst Freigang <slugmoggle@gmx.de>
2018-01-06 20:21:47 +0100

    Making input-file and cue-file a requirement.

--- src/makelp.py
+++ src/makelp.py
@@ -241,8 +241,8 @@ def main():
 	parser = argparse.ArgumentParser(description="With makelp you can extract audio clips from an audio-source. That source can be a ripped audio-track from a YoutTube video. All track infos, like start-time, end-time and song name, need to be in a seperate text file. Look for a detailed help at https://github.com/horstfreigang/makelp.", add_help=False)
 
 	required = parser.add_argument_group("Required arguments");
-	required.add_argument("-i", "--input", dest="audio_source", metavar="", help="Audio source file.", type=str, required=False)
-	required.add_argument("-c", "--cue", dest="cue_file", metavar="", help="Plain text file with album and track infos. See *URL* for more information.", type=str, required=False)
+	required.add_argument("-i", "--input", dest="audio_source", metavar="", help="Audio source file.", type=str, required=True)
+	required.add_argument("-c", "--cue", dest="cue_file", metavar="", help="Plain text file with album and track infos. See *URL* for more information.", type=str, required=True)
 
 	optional = parser.add_argument_group('Optional arguments');
 	optional.add_argument("-h", "--help", action="help", help="Show this help message.")
